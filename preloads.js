e2378de672b2d5d5303025f94e656f41ff0a910f
Iuri Gabriel <iuri_gabriel@hotmail.com>
2021-09-28 13:45:05 -0300

    add game's sound

--- preloads.js
+++ preloads.js
@@ -1,7 +1,11 @@
-//images
+//varImages&Sounds
 let cowImage;
 let streetImage;
 let greenCarImage; let yellowCarImage; let blackCarImage;
+
+let soundBackground;
+let soundCollide;
+let soundScore;
 
 function preload(){
   cowImage = loadImage("images/ator-1.png");
@@ -10,4 +14,8 @@ function preload(){
   yellowCarImage = loadImage("images/carro-3.png");
   blackCarImage = loadImage("images/carro-2.png");
   carsImages = [greenCarImage, yellowCarImage, blackCarImage, greenCarImage, yellowCarImage, blackCarImage];
+
+  soundBackground = loadSound("sounds/trilha.mp3");
+  soundCollide = loadSound("sounds/colidiu.mp3");
+  soundScore = loadSound("sounds/pontos.wav");
 }
